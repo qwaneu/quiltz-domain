9bcf2d7fee326ec59b3a6a9a5301201b0dff607d
rob <rob@qwan.eu>
2021-07-26 15:07:36 +0200

    updated tests for results to use hamcrest

--- tests/quiltz/domain/test_results.py
+++ tests/quiltz/domain/test_results.py
@@ -7,10 +7,10 @@ class ResultContract:
         raise NotImplementedError('Not Implemented Yet')
 
     def test_passed_attributes_can_be_obtained_as_attribute(self, result_class):
-        assert result_class(some_random_attribute="some_value").some_random_attribute == "some_value"
+        assert_that(result_class(some_random_attribute="some_value").some_random_attribute, equal_to("some_value"))
 
     def test_passed_attributes_end_up_in_the_body(self, result_class):
-        assert result_class(some_random_attribute="some_value").body == dict(some_random_attribute="some_value")
+        assert_that(result_class(some_random_attribute="some_value").body, equal_to(dict(some_random_attribute="some_value")))
     
     def test_can_add_an_attribute(self, result_class):
         assert_that(result_class().with_attributes(some_other_attribute="some_other_value").some_other_attribute, equal_to("some_other_value"))
@@ -27,18 +27,18 @@ class TestFailure(ResultContract):
         return Failure
 
     def test_map_on_failure_returns_same_failure(self):
-        result = Failure(message = 'Fail').map(lambda r: Success(value = 10))
-        assert result.message == 'Fail'
+        result = Failure(message='Fail').map(lambda r: Success(value=10))
+        assert_that(result, equal_to(Failure(message='Fail')))
 
     def test_do_on_failure_is_not_executed(self):
         side_effect = SideEffect()
         result = Failure(message = 'Fail').do(lambda result: side_effect.execute() )
-        assert not side_effect.executed
-        assert result.message == 'Fail'
+        assert_that(side_effect.executed, is_not(True))
+        assert_that(result, equal_to(Failure(message='Fail')))
 
     def test_or_fail_with_replaces_failure(self):
         result = Failure(message = 'Fail').or_fail_with(value = 0)
-        assert result.value == 0
+        assert_that(result, equal_to(Failure(reason=Failure(message='Fail'), value=0)))
         
 class TestSuccess(ResultContract):
     @pytest.fixture
@@ -47,17 +47,17 @@ class TestSuccess(ResultContract):
 
     def test_map_on_success_returns_new_result(self):
         result = Success(message = 'Success').map(lambda r: Success(value = 10))
-        assert result.value == 10
+        assert_that(result, equal_to(Success(value=10)))
 
     def test_do_on_success_is_executed(self):
         side_effect = SideEffect()
         result = Success(message = 'Success').do(lambda result: side_effect.execute() )
-        assert side_effect.executed
-        assert result.message == 'Success'
+        assert_that(side_effect.executed, is_(True))
+        assert_that(result, equal_to(Success(message='Success')))
 
     def test_or_fail_with_does_not_affect_success(self):
         result = Success(message = 'Success').or_fail_with(value = 0)
-        assert result.message == 'Success'
+        assert_that(result, equal_to(Success(message='Success')))
 
 class TestPartialSucces(ResultContract):
     @pytest.fixture
