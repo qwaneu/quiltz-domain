0e07ad1c1be41c4a71172063be1e1221d3a14eab
rob <rob@qwan.eu>
2023-04-23 11:56:39 +0200

    removed ls to an non existing directory

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ python -m venv venv
 ./run_tests.sh
 pip install -r requirements/build.txt
 pip list
-ls /opt/hostedtoolcache/Python/3.8.15/x64/lib/python3.8/site-packages/
 python setup.py sdist bdist_wheel
